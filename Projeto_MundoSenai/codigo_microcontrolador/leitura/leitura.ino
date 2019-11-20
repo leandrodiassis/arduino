fbc7329fcc8c647ad6bc08a78efade7375df335f
leandrodiassis <leandroding@yahoo.com>
2019-11-20 12:25:34 -0300

    11/20/2019_12h25

--- Projeto_MundoSenai/codigo_microcontrolador/leitura/leitura.ino
+++ Projeto_MundoSenai/codigo_microcontrolador/leitura/leitura.ino
@@ -3,38 +3,26 @@
  
 #define SS_PIN 10
 #define RST_PIN 9
-int c = 0; 
-char st[20];
-
 MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
-
-//entrada digitais
 const int releEsq = 2; //rele 1
 const int releDir = 3; // rele 2
 const int pinSensor = 4; // pino do sensor
 const int pinFonte = 5; // pino da fonte do sensor
 const int buzzer = 6;
-int comumSensor = A5;
-int receptorAbertura = 7;
-int receptorFechamento = 8;
+int c = 0;
+ 
+char st[20];
  
 void setup() 
 {
   Serial.begin(9600);   // Inicia a serial
   SPI.begin();      // Inicia  SPI bus
   mfrc522.PCD_Init();   // Inicia MFRC522
-  
   pinMode(releEsq, OUTPUT);
   pinMode(releDir, OUTPUT);
   pinMode(buzzer, OUTPUT);
   pinMode(pinSensor, INPUT);
   pinMode(pinFonte, OUTPUT);
-  
-  pinMode(comumSensor, INPUT);
-  pinMode(receptorAbertura, OUTPUT);
-  pinMode(receptorFechamento, OUTPUT);
-  
-  
   
   Serial.println("Aproxime o seu cartao do leitor...");
   Serial.println();
@@ -49,11 +37,6 @@ void loop()
   digitalWrite(pinFonte, LOW); // sensor desligado por padrao
   digitalWrite(releEsq, HIGH); // rele esquerdo desligado por padrao
   digitalWrite(releDir, HIGH); // rele direito desligado por padrao
-  digitalWrite(receptorAbertura, HIGH);
-  digitalWrite(receptorFechamento, HIGH);
-  //int test = analogRead(comumSensor);
-  //Serial.println(test);
-  
   
   // Look for new cards
   if ( ! mfrc522.PICC_IsNewCardPresent()) 
@@ -106,28 +89,10 @@ void loop()
  
 
 int abrirPortao(){
-  
   Serial.println("Abrindo Portao");
-  int estadoPortao;
   digitalWrite(releDir, LOW);
-  //delay(200);
-  
-  digitalWrite(releDir, LOW);
-  delay(100);
-  estadoPortao = analogRead(comumSensor);
-  
-  /*if(estadoPortao <2){
-      digitalWrite(releDir, LOW);
-      delay(500);
-  }*/
-  do{
-    digitalWrite(releDir, LOW);
-    delay(100);
-    estadoPortao = analogRead(comumSensor);
-    }while(estadoPortao < 2);
-  
-  
-  digitalWrite(releDir, HIGH);  
+  delay(1000);  
+  digitalWrite(releDir, HIGH);
   return 0;
 }
 
@@ -135,7 +100,6 @@ int fecharPortao(){
    
    int j = 0; 
    int tempoPortao = 40;
-   int estadoPortao;
      
    
   Serial.println("Processando fechamento");
@@ -172,26 +136,13 @@ int fecharPortao(){
       delay(400);
       tone(buzzer,1200,200);
       delay(800);
-      
-      // impulso para o sensor desencostar
-      digitalWrite(releEsq,LOW);
-      delay(100);
+     
 
       loopSensor: ;
      do{            
-        
-        estadoPortao = analogRead(comumSensor);
-    
-        if(estadoPortao <2){
-            digitalWrite(releEsq,LOW);
-            delay(100);
-            } else{
-              goto terminar;
-            }
-            
-    
-         /*if(j <= tempoPortao){ // quando o portao fechar completamente
-                   
+          
+         if(j <= tempoPortao){ // quando o portao fechar completamente
+            estadoSensor = digitalRead(pinSensor);         
             Serial.println("loop enquanto nao detecta nada");
             digitalWrite(releEsq,LOW);         
           
@@ -201,15 +152,14 @@ int fecharPortao(){
             goto terminar;
             }
           
-        j++;*/
-         estadoSensor = digitalRead(pinSensor); 
+        j++;
       }while(estadoSensor); // enquanto nao detectar      
     }
 
 
 
 
-     
+      if(j<tempoPortao){
       if(!estadoSensor){ // sensor detectou algo enquanto fechava
        
         Serial.println("algo foi detectado enquanto a porta fechava");
@@ -226,7 +176,7 @@ int fecharPortao(){
                 
                       
         }
-      
+      }
       terminar: ;
       Serial.println("chegou no fim");
       return 0;
